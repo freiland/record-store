6885d9e4b665c93cf61136de4b1e15ffb95c7b43
Forrest Reiland <you@example.com>
2020-08-10 10:15:01 -0700

    add script.js and index.html

---
+++ calculator/js/scipt.js
@@ -0,0 +1,6 @@
+function add(number1, number2){
+  return number1 + number2;
+  }
+  const number1 = parseInt(promt("Enter a number:"));
+  const number2 = parseInt(promt('Enter another number:'));
+  alert(add(10,5));
